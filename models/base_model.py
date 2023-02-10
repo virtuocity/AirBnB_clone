ec3bf69abb21e099ec72953e62eb0be98581b6ad
Buno Woche <bunogache@gmail.com>
2023-02-10 17:36:29 +0300

    Dict updates

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,26 @@ import json
 
 class BaseModel:
     """class base model"""
-    def __init__(self, id, created_at, update_at):
+    def __init__(self, my_number, name, id, created_at, updated_at):
+        self.my_number = my_number
+        self.name = name
         self.id = id
         self.created_at = created_at
-        self.update_at = update_at
+        self.update_at = updated_at
     
     def __str__(self):
-        return "" % (self.__class__, self.id, self.__dict__)
+        print("{} {} {}".format((self.__class__, self.id, self.to_dict)))
     
     def to_dict(self):
         dict = {}
         for key, val in self.__dict__.items():
             dict[key] = val
         return dict
+    
+    @property
+    def save(self):
+        """ Update updated at time"""
+        return self.update_at
+
 
     
