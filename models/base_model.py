d9c62f9e31da9678278bd8b9bca28fac932230ff
Buno Woche <bunogache@gmail.com>
2023-02-11 12:48:13 +0300

    pycodestyle corrections

--- models/base_model.py
+++ models/base_model.py
@@ -7,26 +7,25 @@ import json
 
 class BaseModel:
     """class base model"""
-    def __init__(self, my_number, name, id, created_at, updated_at):
+
+    def __init__(self, my_number=0, name="", id=uuid4(),
+                 created_at=date.now(), updated_at=date.now()):
         self.my_number = my_number
         self.name = name
         self.id = id
         self.created_at = created_at
         self.update_at = updated_at
-    
+
     def __str__(self):
-        print("{} {} {}".format((self.__class__, self.id, self.to_dict)))
-    
+        print("{} {} {}".format(self.__class__, self.id, self.to_dict))
+
     def to_dict(self):
         dict = {}
         for key, val in self.__dict__.items():
             dict[key] = val
         return dict
-    
+
     @property
     def save(self):
         """ Update updated at time"""
         return self.update_at
-
-
-    
