757168bc8543f71d89bc56e9dfa82b3fb4595650
Buno Woche <bunogache@gmail.com>
2023-02-12 16:01:50 +0300

    Test base models

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import inspect
 import models
 import pep8
-import uninttest
+import unittest
 
 BaseModel = models.base_model.BaseModel
 
@@ -16,4 +16,41 @@ class TestBaseModelDocs(unittest.TestCase):
 	
 	all_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
 
+	def test_doc_file(self):
+		"""... documentation for the file"""
+		expected = '\nBaseModel Class of Models Module\n'
+		actual = models.base_model.__doc__
+		self.assertEqual(expected, actual)
+	
+	def test_doc_class(self):
+		"""... documentation for the class"""
+		expected = ('\n        attributes and functions for BaseModel class\n'
+                    '    ')
+		actual = BaseModel.__doc__
+		self.assertEqual(expected, actual)
 
+	def test_all_function_docs(self):
+		"""... tests for ALL DOCS for all functions in db_storage file"""
+		all_functions = TestBaseModelDocs.all_funcs
+		for function in all_functions:
+			self.assertIsNotNone(function[1].__doc__)
+
+	def test_pep8_base_model(self):
+		"""... base_model.py conforms to PEP8 Style"""
+		pep8style = pep8.StyleGuide(quiet=True)
+		errors = pep8style.check_files(['models/base_model.py'])
+		self.assertEqual(errors.total_errors, 0, errors.messages)
+
+	def test_file_is_executable(self):
+		"""... tests if file has correct permissions so user can execute"""
+		file_stat = stat('models/base_model.py')
+		permissions = str(oct(file_stat[0]))
+		actual = int(permissions[5:-2]) >= 5
+		self.assertTrue(actual)
+
+
+if __name__ == '__main__':
+    """
+    MAIN TESTS
+    """
+    unittest.main
