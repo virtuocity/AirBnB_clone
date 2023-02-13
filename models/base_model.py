91efa14ded209e64e44b3ae29515a9486858ddf1
Buno Woche <bunogache@gmail.com>
2023-02-13 16:07:58 +0300

    Addition of datetime isoformat method

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-"""AirBnB clone - the console Base class"""
 from uuid import uuid4
 from datetime import datetime as date
 
@@ -7,36 +5,13 @@ from datetime import datetime as date
 class BaseModel:
     """class base model"""
 
-    def __init__(self, *args, **kwargs):
-        """
-            instantiation of new BaseModel object
-        """
-        if kwargs:
-            self.__set_attribs(kwargs)
-        else:
-            self.id = str(uuid4())
-            self.created_at = date.utcnow()
-
-    def __set_attribs(self, attrib_dict):
-        """
-            private method: converts attr_dict values to python class attributes
-        """
-        if 'id' not in attrib_dict:
-            attrib_dict['id'] = str(uuid4())
-        if 'created_at' not in attrib_dict:
-            attrib_dict['created_at'] = date.utcnow()
-        elif not isinstance(attrib_dict['created_at'], date):
-            attrib_dict['created_at'] = date.strptime(
-                attrib_dict['created_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        if 'updated_at' not in attrib_dict:
-            attrib_dict['updated_at'] = date.utcnow()
-        elif not isinstance(attrib_dict['updated_at'], date):
-            attrib_dict['updated_at'] = date.strptime(
-                attrib_dict['updated_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        for attr, val in attrib_dict.items():
-            setattr(self, attr, val)
+    def __init__(self, my_number=0, name="", updated_at=date.utcnow(), id=str(uuid4()),
+                 created_at=date.utcnow()):
+        self.my_number = my_number
+        self.name = name
+        self.updated_at = updated_at
+        self.id = id
+        self.created_at = created_at
 
     def __str__(self):
         """
@@ -44,15 +19,25 @@ class BaseModel:
         """
         class_name = type(self).__name__
         return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
-    
+
+    def insert_at_nth_position(dictitonary, n, key, value):
+        if n < 0 > len(dictitonary):
+            raise ValueError("Index out of range")
+        items = list(dictitonary.items())
+        items.insert(n, (key, value))
+        return dict(items)
 
     def to_dict(self):
         """Dict representation of object"""
         dict = {}
         for key, val in self.__dict__.items():
-
-            dict[key] = val
-        dict["__class"] = self.__class__
+            if key is "updated_at":
+                dict["updated_at"] = date.isoformat(val)
+            elif key is "created_at":
+                dict["created_at"] = date.isoformat(val)
+            else:
+                dict[key] = val
+        dict["__class__"] = self.__class__.__name__
         return dict
 
     def save(self):
